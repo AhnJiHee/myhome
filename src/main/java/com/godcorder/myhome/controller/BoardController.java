ba82dd0ed23eb3044fbd60b5f2fcc35573e53fb3
ahnjihee <sor890@gmail.com>
2021-11-15 16:20:50 +0900

    1115

--- src/main/java/com/godcorder/myhome/controller/BoardController.java
+++ src/main/java/com/godcorder/myhome/controller/BoardController.java
@@ -1,14 +1,65 @@
 package com.godcorder.myhome.controller;
 
+import com.godcorder.myhome.model.Board;
+import com.godcorder.myhome.repository.BoardRepository;
+import com.godcorder.myhome.validator.BoardValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.querydsl.QPageRequest;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
+    @Autowired
+    private BoardRepository boardRepasitory;
+
+    @Autowired
+    private BoardValidator boardValidator;
+
     @GetMapping("/list")
-    public String list(){
+    public String list(Model model, @PageableDefault(size = 2) Pageable pageable, @RequestParam(required = false, defaultValue = "") String searchText){
+ //       Page<Board> boards = boardRepasitory.findAll(pageable);
+        Page<Board> boards = boardRepasitory.findByTitleContainingOrContentContaining(searchText, searchText, pageable);
+
+
+        int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
+        int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        model.addAttribute("boards", boards);
         return  "board/list";
     }
+
+    @GetMapping("/form")
+    public String form(Model model, @RequestParam(required = false) Long id){
+        if(id == null){
+            model.addAttribute("board", new Board());
+        }else{
+            Board board = boardRepasitory.findById(id).orElse(null);
+            model.addAttribute("board", board);
+        }
+
+        return "board/form";
+    }
+
+    @PostMapping("/form")
+    public String postForm(@Valid Board board, BindingResult bindingResult){
+        boardValidator.validate(board, bindingResult);
+        if(bindingResult.hasErrors()){
+            return "board/form";
+        }
+        boardRepasitory.save(board);
+        return "redirect:/board/list";
+    }
+
 }
---
+++ src/main/java/com/godcorder/myhome/repository/BoardRepository.java
@@ -0,0 +1,18 @@
+package com.godcorder.myhome.repository;
+
+import com.godcorder.myhome.model.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.ui.Model;
+
+import java.util.List;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    List<Board> findByTitle(String title);
+    List<Board> findByTitleOrContent(String title, String content);
+
+    Page<Board> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
+
+}
